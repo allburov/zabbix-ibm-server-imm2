395ee3ff435d22c62650a081d9c746c2981070ec
Aleksey Burov <allburov@gmail.com>
2016-08-23 12:55:08 +0700

    Remove hardcode ip

--- getFanSpeed.rb
+++ getFanSpeed.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/ruby
 community=ARGV[0]
 fannumber=ARGV[1]
+serveraddr=ARGV[2]
 #fannumber=1
-execstr="snmpwalk -c #{community} -v 1 192.168.5.250 1.3.6.1.4.1.2.3.51.3.1.3.2.1.3.#{fannumber}"
+execstr="snmpwalk -c #{community} -v 1 #{serveraddr} 1.3.6.1.4.1.2.3.51.3.1.3.2.1.3.#{fannumber}"
 strresult=`#{execstr}`
 fanspeed=strresult.slice(/[\d]*%/)
 if fanspeed.nil?
